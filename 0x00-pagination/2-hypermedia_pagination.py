85ac24bcf897f4749bde662bed75c17d8bbd6f60
mberrouk <mberrouk@student.1337.ma>
2024-09-25 22:40:17 +0100

    implement get_hyper

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""
+Implementing Simple Pagination.
+"""
+
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Found a start index and end index.
+    """
+    start_index = (page - 1) * page_size
+    end_index = ((page - 1) * page_size) + page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ Retrive data
+        """
+        assert type(page) is int and type(page_size) is int
+        assert page > 0 and page_size > 0
+        beg, end = index_range(page, page_size)
+        data = self.dataset()
+        if beg > len(data) or end > len(data):
+            return []
+        return data[beg:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict[str, str]:
+        assert type(page) is int and type(page_size) is int
+        assert page > 0 and page_size > 0
+        beg, end = index_range(page, page_size)
+        data = self.dataset()
+        # if beg > len(data) or end > len(data):
+        #     return []
+        dictData = {
+            "page_size": len(data),
+            "page": page,
+            "data": data[beg:end],
+            "next_page": end + 1 if end < len(data) else None,
+            "prev_page": beg - 1 if beg > 1 else None,
+            "total_page": len(data)
+        }
+        return dictData
