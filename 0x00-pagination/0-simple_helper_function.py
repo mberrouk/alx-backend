4b8fbcede5f10d2179426c350fac0f2f36cf859a
mberrouk <mberrouk@student.1337.ma>
2024-09-25 20:38:49 +0100

    init

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,7 +3,8 @@
 Implementing Simple Pagination.
 """
 
-def index_range(page:int, page_size:int) -> tuple[int, int]:
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
     """
     Found a start index and end index.
     """
