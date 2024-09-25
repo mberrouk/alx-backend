bb02cd35c3c686f4e747889a3bb4afc9ae9fb0ff
mberrouk <mberrouk@student.1337.ma>
2024-09-25 20:43:02 +0100

    init

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -9,5 +9,5 @@ def index_range(page: int, page_size: int) -> tuple[int, int]:
     Found a start index and end index.
     """
     start_index = (page - 1) * page_size
-    end_index = page * page_size
+    end_index = ((page - 1) * page_size) + page_size
     return (start_index, end_index)
