8813de58105997020f32f5bc66cdeb87a24ffb36
mberrouk <mberrouk@student.1337.ma>
2024-09-25 20:34:16 +0100

    init

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -10,7 +10,3 @@ def index_range(page:int, page_size:int) -> tuple[int, int]:
     start_index = (page - 1) * page_size
     end_index = page * page_size
     return (start_index, end_index)
-
-
-print(index_range(3, 1))
-print(type(index_range(1, 1)))
